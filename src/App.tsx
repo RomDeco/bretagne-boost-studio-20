0ea9a41cb23a41cf508dc02938d4df38eeaa93f8
Aurelien <biowhazarde@gmail.com>
2025-09-08 11:59:17 +0200

    rebuild + modify hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CreationGraphique from "./pages/CreationGraphique";
 import Enseignes from "./pages/Enseignes";
@@ -13,6 +12,8 @@ import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Legal from "./pages/Legal";
 import NotFound from "./pages/NotFound";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 const queryClient = new QueryClient();
 
@@ -21,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/creation-graphique" element={<CreationGraphique />} />
@@ -32,12 +33,12 @@ const App = () => (
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/legal" element={<Legal />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
 
 export default App;
